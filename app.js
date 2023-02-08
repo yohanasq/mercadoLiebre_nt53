e92955f4aac9e4093207e1e093ac97d8fed48997
yohanasq <yohanasanchezq@gmail.com>
2023-02-07 20:21:28 -0500

    mi primer commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
