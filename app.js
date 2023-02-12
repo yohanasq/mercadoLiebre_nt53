6f7b1bfebf1b6c7bfbcb62c7e61bd880c764b4ea
yohanasq <yohanasanchezq@gmail.com>
2023-02-12 13:30:01 -0500

    agregando estilos y modificando carpeta public

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
-const port = 3000;
+
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 });
